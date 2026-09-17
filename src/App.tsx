b43dd6a3ef5df5c92147f66a0832bedfd96d292b
surieltech <grandesuriel@gmail.com>
2026-09-16 20:25:41 -0400

    first commit: initial setup and configured routes

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom"
+
+function App() {
+
+  return (
+    <div className="app">
+      <h1>GitHub Finder</h1>
+      <Outlet></Outlet>
+    </div>
+  )
+}
+
+export default App
